59f2fa3527f827bd3aaf8a0228f673035118d421
PraneethKumarT <67374533+PraneethKumarT@users.noreply.github.com>
2026-07-10 06:07:36 -0500

    Add: find-minimum-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-5.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        minVal = nums[0]
+        low, high = 0, len(nums)-1
+        
+        while low <= high:
+            mid = (low+high)//2
+
+            if nums[mid] >= nums[low]:
+                minVal = min(minVal, nums[low])
+                low = mid+1
+            elif nums[mid] < nums[high]:
+                minVal = min(minVal, nums[mid])
+                high = mid - 1
+        
+        return minVal
+        
+
+
+"""
+
+numbers are unique
+min element - O(logn)
+
+        0 1 2 3 4 5
+input: [3,4,5,6,1,2]. 
+
+
+Record min and search the unsorted split
+mid > low - search right
+mid < high - search left
+"""
